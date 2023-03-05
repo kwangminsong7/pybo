aca760cd2998c1a93b095c492780f34bb354068b
송광민 <somm7301@naver.com>
2023-03-05 20:19:03 +0900

    prod.py 도메인 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['13.125.35.162']
+ALLOWED_HOSTS = ['13.125.35.162', 'skmwebdev.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
